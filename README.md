3041ace8650743a8fe00afe116b59460ec6e3123
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2018-10-26 13:06:20 +0200

    version 1

--- README.md
+++ README.md
@@ -2,12 +2,67 @@
 
 [![npm version](http://badge.fury.io/js/c0nfig.svg)](http://badge.fury.io/js/c0nfig)
 [![Dependency Status](http://david-dm.org/voronianski/c0nfig.svg)](http://david-dm.org/voronianski/c0nfig)
-<!-- [![Download Count](http://img.shields.io/npm/dm/c0nfig.svg?style=flat)](http://www.npmjs.com/package/c0nfig) -->
 
-> Use [node-config-boilerplate](https://github.com/voronianski/node-config-boilerplate) simply by `require('c0nfig')`.
+> Require local environment based configs as if they are in `node_modules`.
+
+## Install
 
 ```bash
 npm install c0nfig --save
+```
+
+## Usage
+
+Create configs for every app environment just putting the env name as prefix of the file name:
+
+```bash
+app-folder/config:~$ ls -a
+
+production.config.js
+development.config.js
+# etc.
+```
+
+Export some configuration data for every environment like:
+
+```js
+// development.config.js
+module.exports = {
+  title: 'BLITZKRIEG BOP (STAGING)',
+  apiUrl: 'https://staging.example.org/api'
+};
+```
+
+```js
+// production.config.js
+module.exports = {
+  title: 'BLITZKRIEG BOP (PRODUCTION)',
+  apiUrl: 'https://example.org/api'
+};
+```
+
+Start your app with proper `NODE_ENV` (if not provided it will grab `development.config.js` by default), then require/import `c0nfig` in your source code and use the data:
+
+```js
+// src/app.js
+const config = require('c0nfig');
+const request = require('superagent');
+
+request.get(config.apiUrl).then(res => { ... });
+```
+
+### Template tags
+
+You are able to use template tags like `$(configProperty.childProperty)` to point to specific properties of config:
+
+```js
+module.exports = {
+  title: 'BLITZKRIEG BOP!',
+  http: {
+    port: process.env.PORT || 8080,
+    url: process.env.URL || 'http://0.0.0.0:$(http.port)'
+  }
+};
 ```
 
 ---
--- src/config.js
+++ src/config.js
@@ -1,46 +1,60 @@
 const env = process.env.NODE_ENV || 'development';
 
-var util = require('util');
-var fileName = util.format('%s.config.js', env);
-var config = require('../../config/' + fileName);
+const path = require('path');
+const fileName = `${env}.config.js`;
+const filePath = path.resolve(__dirname, `../../config/${fileName}`);
+
+console.log(1, filePath);
+
+const config = require(filePath);
 
 config.env = env;
 populateEachConfig(config);
 
-function replaceTemplateTags (key) {
-    if (!key.length) {
-        return config;
-    }
-    if (!Array.isArray(key)) {
-        return replaceTemplateTags(key.split('.'));
-    }
-    return key.reduce((result, i) => result && result[i], config);
+function replaceTemplateTags(key) {
+  if (!key.length) {
+    return config;
+  }
+
+  if (!Array.isArray(key)) {
+    return replaceTemplateTags(key.split('.'));
+  }
+
+  return key.reduce((result, i) => result && result[i], config);
 }
 
-function coerce (value) {
-    if (typeof value !== 'string') {
-        return value;
-    }
-    value = value.replace(/\$\(([^\)]+)\)/g, (_, key) => replaceTemplateTags(key));
-    if (/^\d+$/.test(value)) {
-        return parseInt(value, 10);
-    }
-    if (value === 'false') {
-        return false;
-    }
-    if (value === 'true') {
-        return true;
-    }
+function coerce(value) {
+  if (typeof value !== 'string') {
     return value;
+  }
+
+  value = value.replace(/\$\(([^\)]+)\)/g, (_, key) => {
+    return replaceTemplateTags(key);
+  });
+
+  if (/^\d+$/.test(value)) {
+    return parseInt(value, 10);
+  }
+
+  if (value === 'false') {
+    return false;
+  }
+
+  if (value === 'true') {
+    return true;
+  }
+
+  return value;
 }
 
-function populateEachConfig (cfg) {
-    Object.keys(cfg).forEach(key => {
-        if (typeof cfg[key] === 'object' && cfg[key]) {
-            return populateEachConfig(cfg[key]);
-        }
-        cfg[key] = coerce(cfg[key]);
-    });
+function populateEachConfig(cfg) {
+  Object.keys(cfg).forEach(key => {
+    if (typeof cfg[key] === 'object' && cfg[key]) {
+      return populateEachConfig(cfg[key]);
+    }
+
+    cfg[key] = coerce(cfg[key]);
+  });
 }
 
 export default config;
